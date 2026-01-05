08b0c4427976960b0f6c63ee8756793551bbe755
yiran529 <yiran0834@gmail.com>
2026-01-05 07:10:21 +0000

    Being able to run, but with obvious bugs

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -21,7 +21,10 @@
 ssize_t sys_user_print(const char* buf, size_t n) {
   // buf is now an address in user space of the given app's user stack,
   // so we have to transfer it into phisical address (kernel is running in direct mapping).
-  assert( current );
+  int hid = read_tp();
+  process* p = current[hid];
+
+  assert(p);
   
   // sprint("\n=== KERNEL MODE: Manual Translation Required ===\n");
   // sprint("buf (user VA): 0x%lx\n", (uint64)buf);
@@ -30,9 +33,8 @@ ssize_t sys_user_print(const char* buf, size_t n) {
   // sprint("Why manual? satp != user_pagetable, so MMU can't auto-translate buf!\n");
   // sprint("Calling user_va_to_pa() to manually walk user page table...\n");
   
-  char* pa = (char*)user_va_to_pa((pagetable_t)(current->pagetable), (void*)buf);
+  char* pa = (char*)user_va_to_pa((pagetable_t)(p->pagetable), (void*)buf);
   // sprint(pa);
-  int hid = read_tp();
   // Add hartid to prints so we can distinguish which hart produced the user message.
   sprint("hartid = %d: %s\n", hid, pa);
   // sprint("Translated PA: 0x%lx\n", (uint64)pa);
@@ -89,14 +91,17 @@ ssize_t sys_user_exit(uint64 code) {
 // maybe, the simplest implementation of malloc in the world ... added @lab2_2
 //
 uint64 sys_user_allocate_page() {
+  int hid = read_tp();
+  process* p = current[hid];
+
   void* pa = alloc_page();
   /*取当前“用户简单堆”指针的当前位置作为本次分配的虚拟页起始地址。
   g_ufree_page 是一个单调递增游标，表示下一个可用的用户虚拟地址（位于用户进程的“自由区”起点之后）*/
   uint64 va = g_ufree_page;
   g_ufree_page += PGSIZE;
-  user_vm_map((pagetable_t)current->pagetable, va, PGSIZE, (uint64)pa,
+  user_vm_map((pagetable_t)p->pagetable, va, PGSIZE, (uint64)pa,
          prot_to_type(PROT_WRITE | PROT_READ, 1));
-  sprint("hartid = ?: vaddr 0x%x is mapped to paddr 0x%x\n", va, pa);
+  sprint("hartid = %d: vaddr 0x%x is mapped to paddr 0x%x\n", hid, va, pa);
   return va;
 }
 
@@ -104,7 +109,10 @@ uint64 sys_user_allocate_page() {
 // reclaim a page, indicated by "va". added @lab2_2
 //
 uint64 sys_user_free_page(uint64 va) {
-  user_vm_unmap((pagetable_t)current->pagetable, va, PGSIZE, 1);
+  int hid = read_tp();
+  process* p = current[hid];
+
+  user_vm_unmap((pagetable_t)p->pagetable, va, PGSIZE, 1);
   return 0;
 }
 
